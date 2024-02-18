4d6a329545f4a6de81e7ede3f3d5c89e7ef38306
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2024-02-17 16:12:44 -0800

    Add calendly link to signup function

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ async def root():
 
 @app.post("/signup/")
 async def signup(user: User):
-    query = "INSERT INTO users(name, senior, interests) VALUES (:name, :senior, :interests)"
-    values = {"name": user.name, "senior": user.senior, "interests": user.interests}
+    query = "INSERT INTO users(name, senior, interests) VALUES (:name, :senior, :interests, :calendly)"
+    values = {"name": user.name, "senior": user.senior, "interests": user.interests, "calendly": user.calendly}
     await database.execute(query=query, values=values)
     return {"message": f"User {user.name} signed up successfully!"}
 
